68d08242a9c65830dc60c5608eb0abff75e6b3be
Tim Hutton <tihutt@microsoft.com>
2018-07-14 02:05:47 +0100

    Reuse open url if available.

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -32,6 +32,29 @@ self.addEventListener('notificationclick', function(event) {
   clickedNotification.close();
 
   const page = 'index.html';
-  const promiseChain = clients.openWindow(page);
+  const urlToOpen = new URL(page, self.location.origin).href;
+
+  const promiseChain = clients.matchAll({
+    type: 'window',
+    includeUncontrolled: true
+  })
+  .then((windowClients) => {
+    let matchingClient = null;
+
+    for (let i = 0; i < windowClients.length; i++) {
+      const windowClient = windowClients[i];
+      if (windowClient.url === urlToOpen) {
+        matchingClient = windowClient;
+        break;
+      }
+    }
+
+    if (matchingClient) {
+      return matchingClient.focus();
+    } else {
+      return clients.openWindow(urlToOpen);
+    }
+  });
+
   event.waitUntil(promiseChain);
 });
